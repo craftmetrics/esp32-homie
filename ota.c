00ced515828e64876e2a30afb7cf84aa27405401
Luke Cyca <me@lukecyca.com>
2018-04-07 12:47:37 -0700

    Add OTA status messages

--- ota.c
+++ ota.c
@@ -30,15 +30,11 @@ int download_callback(request_t *req, char *data, int len)
     static esp_ota_handle_t update_handle;
     static const esp_partition_t *update_partition;
 
-    if (req->response->status_code != 200) {
-        ESP_LOGE(TAG, "HTTP %d: %s", req->response->status_code, data);
-        return -1;
-    }
-
     if (total_len == -1) {
         tmp = req_list_get_key(req->response->header, "Content-Length");
         if (!tmp) {
             ESP_LOGE(TAG, "Content-Length not found");
+            homie_publish("$implementation/ota/status", "500 no Content-Length");
             return -1;
         }
         total_len = atoi(tmp->value);
@@ -48,6 +44,7 @@ int download_callback(request_t *req, char *data, int len)
         update_partition = esp_ota_get_next_update_partition(NULL);
         if (update_partition == NULL) {
             ESP_LOGE(TAG, "Invalid or no OTA data partition found");
+            homie_publish("$implementation/ota/status", "500 no ota partition");
             return -1;
         }
         ESP_LOGI(TAG, "Writing to partition type %d subtype %d (offset 0x%08x)",
@@ -56,29 +53,41 @@ int download_callback(request_t *req, char *data, int len)
         err = esp_ota_begin(update_partition, total_len, &update_handle);
         if (err != ESP_OK) {
             ESP_LOGE(TAG, "esp_ota_begin error: %d", err);
+            homie_publishf("$implementation/ota/status", "500 esp_ota_begin=%d", err);
             return -1;
         }
     }
 
     err = esp_ota_write(update_handle, (const void *)data, len);
-    if(err != ESP_OK) {
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "esp_ota_write error: %d", err);
+        homie_publishf("$implementation/ota/status", "500 esp_ota_write=%d", err);
         return -1;
     }
     remaining_len -= len;
+
+    // Send status message to indicate OTA progress
+    char buf[32];
+    sprintf(buf, "206 %d/%d", total_len-remaining_len, total_len);
+    homie_publish("$implementation/ota/status", buf);
 
     if (remaining_len <= 0) {
         err = esp_ota_end(update_handle);
         if (err != ESP_OK) {
             ESP_LOGE(TAG, "esp_ota_end error: %d", err);
+            homie_publishf("$implementation/ota/status", "500 esp_ota_end=%d", err);
             return -1;
         }
         err = esp_ota_set_boot_partition(update_partition);
         if (err != ESP_OK) {
             ESP_LOGE(TAG, "esp_ota_set_boot_partition error: %d", err);
+            homie_publishf("$implementation/ota/status", "500 esp_ota_set_boot_partition=%d", err);
             return -1;
         }
         ESP_LOGI(TAG, "OTA Update Complete - rebooting");
+        // Send status message to indicate that OTA is complete
+        homie_publish("$implementation/ota/status", "200");
+        vTaskDelay(3000/portTICK_PERIOD_MS);
         esp_restart();
     }
 
@@ -89,25 +98,7 @@ static void ota_task(void *pvParameter)
 {
     char * url = (char *)pvParameter;
 
-    ESP_LOGI(TAG, "Initiating OTA from %s", url);
-
-    const esp_partition_t *configured = esp_ota_get_boot_partition();
-    const esp_partition_t *running = esp_ota_get_running_partition();
-
-    if ((configured == NULL) || (configured == NULL)) {
-        ESP_LOGE(TAG, "OTA partitions not configured");
-        homie_publish("$implementation/ota/status", "501");
-        ota_deinit(url);
-        return;
-    }
-
-    if (configured != running) {
-        ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
-                 configured->address, running->address);
-        ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
-    }
-    ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
-             running->type, running->subtype, running->address);
+    ESP_LOGI(TAG, "Downloading %s", url);
 
     request_t *req;
     int status;
@@ -118,10 +109,37 @@ static void ota_task(void *pvParameter)
     status = req_perform(req);
     ESP_LOGI(TAG, "req_perform returned %d", status);
     req_clean(req);
+
+    // Report http-related error
+    if (status != 200) homie_publish_int("$implementation/ota/status", status);
+
     ota_deinit(url);
 }
 
 void ota_init(char * url)
 {
+    ESP_LOGI(TAG, "Initiating OTA");
+
+    const esp_partition_t *configured = esp_ota_get_boot_partition();
+    const esp_partition_t *running = esp_ota_get_running_partition();
+
+    // Ensure OTA is configured
+    if ((configured == NULL) || (configured == NULL)) {
+        ESP_LOGE(TAG, "OTA partitions not configured");
+        homie_publish("$implementation/ota/status", "500 no ota partitions");
+        free(url);
+        return;
+    }
+
+    // Check OTA partitions
+    if (configured != running) {
+        ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
+                 configured->address, running->address);
+        ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
+    }
+    ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
+             running->type, running->subtype, running->address);
+
+    // Begin OTA task
     xTaskCreate(&ota_task, "ota_task", 8192, url, 5, NULL);
 }
