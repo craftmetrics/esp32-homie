174093fb9955c0fcd752317620d97323459ecec7
Tomoyuki Sakurai <y@trombik.org>
2019-12-06 10:48:50 +0900

    bugfix: publish mac, ip, and sdk

--- homie.c
+++ homie.c
@@ -439,7 +439,7 @@ static esp_err_t homie_connected()
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("$nodes", QOS_1, RETAINED, nodes));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/$name", QOS_1, RETAINED, CHIP_NAME));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publishf("esp/$type", QOS_1, RETAINED, "rev: %d", chip_info.revision));
-    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/$properties", QOS_1, RETAINED, "uptime,rssi,signal,freeheap"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/$properties", QOS_1, RETAINED, "uptime,rssi,signal,freeheap,mac,ip,sdk"));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/uptime/$name", QOS_1, RETAINED, "Uptime since boot"));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/uptime/$datatype", QOS_1, RETAINED, "integer"));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/rssi/$name", QOS_1, RETAINED, "WiFi RSSI"));
@@ -448,6 +448,16 @@ static esp_err_t homie_connected()
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/signal/$datatype", QOS_1, RETAINED, "integer"));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/freeheap/$name", QOS_1, RETAINED, "Free heap memory"));
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/freeheap/$datatype", QOS_1, RETAINED, "integer"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/mac/$name", QOS_1, RETAINED, "MAC address"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/mac/$datatype", QOS_1, RETAINED, "string"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/mac", QOS_1, RETAINED, mac_address));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/ip/$name", QOS_1, RETAINED, "IP address"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/ip/$datatype", QOS_1, RETAINED, "string"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/ip", QOS_1, RETAINED, ip_address));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/sdk/$name", QOS_1, RETAINED, "SDK version"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/sdk/$datatype", QOS_1, RETAINED, "string"));
+    FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("esp/sdk", QOS_1, RETAINED, esp_get_idf_version()));
+
     FAIL_IF_LESS_THAN_OR_EQUAL_ZERO(homie_publish("$state", QOS_1, RETAINED, "ready"));
 #elif defined(CONFIG_HOMIE_VERSION_2_0_1)
     int msg_id;
