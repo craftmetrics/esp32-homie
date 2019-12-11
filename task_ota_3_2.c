c5592495b4eb91bc2f81da67b998665cff02c6b3
Tomoyuki Sakurai <y@trombik.org>
2019-12-11 12:33:16 +0900

    bugfix: include esp_event.h when 4.x

--- task_ota_3_2.c
+++ task_ota_3_2.c
@@ -10,12 +10,18 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_system.h"
-#include "esp_event_loop.h"
 #include "esp_log.h"
 #include "esp_ota_ops.h"
 #include "esp_http_client.h"
 #include "esp_flash_partitions.h"
 #include "esp_partition.h"
+
+#include "esp_idf_lib_helpers.h"
+#if HELPER_TARGET_VERSION >= HELPER_TARGET_VERSION_ESP32_V4
+#include <esp_event.h>
+#else
+#include <esp_event_loop.h>
+#endif
 
 #include "nvs.h"
 #include "nvs_flash.h"
