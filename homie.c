a323ccd50c91e95103ad3c3eb9dbff329b0501bf
Tomoyuki Sakurai <y@trombik.org>
2020-05-30 14:48:34 +0700

    bugfix: handle unhandled event
    also: fix ESP8266 build

--- homie.c
+++ homie.c
@@ -193,6 +193,11 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
         ESP_LOGI(TAG, "MQTT_EVENT_BEFORE_CONNECT");
         break;
 #endif
+
+#if HELPER_TARGET_IS_ESP32 > 0 && HELPER_TARGET_VERSION >= HELPER_TARGET_VERSION_ESP32_V4
+    case MQTT_EVENT_ANY:
+        break;
+#endif
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
         xEventGroupSetBits(*config->event_group, HOMIE_MQTT_CONNECTED_BIT);
@@ -216,7 +221,6 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
         // Disabled to avoid triggering circular events with remote logging
         //ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
         break;
-
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
 
